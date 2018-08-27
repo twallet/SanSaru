78ac23421e544c3959c0db693441545224833e52
andres restrepo <kadashyx@gmail.com>
2018-08-26 22:35:55 -0300

    Cambio de pregunta de valores de manifieto por rango de edad

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: 'aoc', lastname: 'admin', email: 'chile@agileopen.camp', password: 'passadmin',
             password_confirmation: 'passadmin', admin: true, terms_of_service: 'true')
-Agile.create(name: 'Individuos e interacciones sobre procesos y herramientas')
-Agile.create(name: 'Software funcionando sobre documentación extensiva')
-Agile.create(name: 'Colaboración con el cliente sobre negociación contractual')
-Agile.create(name: 'Respuesta ante el cambio sobre seguir un plan')
+
+Agile.create(name: '18 a 24')
+Agile.create(name: '25 a 34')
+Agile.create(name: '35 a 44 ')
+Agile.create(name: '45 a 54')
+Agile.create(name: '55 a 64')
+Agile.create(name: 'mayor de 65')
 
 AgileRelation.create(name: 'Como hermanos')
 AgileRelation.create(name: 'Mejores amigos')
