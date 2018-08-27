130a2fe2591faec1d549691f37b50daf98bf5ba3
andres restrepo <kadashyx@gmail.com>
2018-08-26 23:38:15 -0300

    add age

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,12 @@ ActiveRecord::Schema.define(version: 2018_07_24_232923) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ages", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "lastname", null: false
